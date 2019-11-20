140a2df3bedfec91dbb04675e8b91eab90ed2dc7
Ryan <rngao97@gmail.com>
2019-11-20 10:20:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    pInner.textContent = "This is really cool!"
+  });
+  
+  const pInner = document.querySelector("p")
+  
